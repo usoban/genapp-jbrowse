b251cd725f064ea1edabaf35cd101ad20366b453
lukaw3d <lukaw3d@gmail.com>
2014-07-10 23:07:50 +0200

    Browser rename to jbrowse

--- static/genapp-jbrowse/app/directives.js
+++ static/genapp-jbrowse/app/directives.js
@@ -28,7 +28,7 @@ angular.module('jbrowse.directives', ['genjs.services'])
          *      :onConnect:     On JBrowse initialize callback.
          *      :afterAdd:      Dict with data types as keys and callback functions as values. Callback is executed after
          *                      given data type is added to the browser.
-         *      :browser:       Directive exposes JBrowse object after connecting
+         *      :jbrowse:       Directive exposes JBrowse object after connecting
          *
          *      API:
          *      :js:func:`addTrack`
@@ -307,7 +307,7 @@ angular.module('jbrowse.directives', ['genjs.services'])
 
                     preConnect();
                     $scope.browser = new Browser($scope.config);
-                    $scope.options.browser = $scope.browser;
+                    $scope.options.jbrowse = $scope.browser;
                     connector();
                 });
             }]
